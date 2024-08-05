98064abbf0c8a1c617ad2444e78bf1f33c9fadd7
QuinnCalzada <calzadaquinn@gmail.com>
2024-08-05 19:24:46 -0400

    Update index.js

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://ebt2idsxwlt4lwy3y34g7far440wyhwc.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://nwnyjdhnb4psrrxsvrp562ok2a0fubwo.lambda-url.us-east-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
